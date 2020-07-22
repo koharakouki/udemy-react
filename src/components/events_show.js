a2ad0946065e77b5370fff38e4518ce54c22d4ed
koharakouki <ko.kohara02@gmail.com>
2020-07-22 12:15:49 +0900

    update event

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -30,7 +30,7 @@ class EventsShow extends Component {
   }
 
   async onSubmit(values) {
-    // await this.props.postEvent(values)
+    await this.props.putEvent(values)
     this.props.history.push('/')
   }
 
@@ -42,14 +42,14 @@ class EventsShow extends Component {
 
 
   render(){
-    const { handleSubmit, pristine, submitting } = this.props
+    const { handleSubmit, pristine, submitting, invalid } = this.props
     return(
       <form onSubmit={handleSubmit(this.onSubmit)}>
         <div><Field label="Title" name="title" type="text" component={this.renderField} /></div>
         <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
 
         <div>
-          <input type="submit" value="Submit" disabled={pristine || submitting } />
+          <input type="submit" value="Submit" disabled={pristine || submitting || invalid } />
           <Link to="/">Cancel</Link>
           <Link to="/" onClick={this.onDeleteClick}>Delete</Link>
         </div>
@@ -73,7 +73,7 @@ const mapStateToProps = (state, ownProps) => {
   return { initialValues: event, event }
 }
 
-const mapDispatchToProps =  ({ deleteEvent, getEvent })
+const mapDispatchToProps =  ({ deleteEvent, getEvent, putEvent })
 
 
 export default connect(mapStateToProps, mapDispatchToProps)(
