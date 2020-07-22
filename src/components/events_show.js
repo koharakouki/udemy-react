88ea276d6dc62d44017dfda57325d7f343986f0c
koharakouki <ko.kohara02@gmail.com>
2020-07-22 11:42:27 +0900

    implement delete feature

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -10,6 +10,7 @@ class EventsShow extends Component {
   constructor(props){
     super(props);
     this.onSubmit = this.onSubmit.bind(this);
+    this.onDeleteClick = this.onDeleteClick.bind(this);
   }
 
   renderField(field){
@@ -28,6 +29,12 @@ class EventsShow extends Component {
     this.props.history.push('/')
   }
 
+  async onDeleteClick(){
+    const { id } = this.props.match.params
+    await this.props.deleteEvent(id)
+    this.props.history.push('/')
+  }
+
 
   render(){
     const { handleSubmit, pristine, submitting } = this.props
@@ -39,6 +46,7 @@ class EventsShow extends Component {
         <div>
           <input type="submit" value="Submit" disabled={pristine || submitting } />
           <Link to="/">Cancel</Link>
+          <Link to="/" onClick={this.onDeleteClick}>Delete</Link>
         </div>
       </form>
     );
@@ -54,10 +62,10 @@ const validate = values => {
 
   return errors
 }
-// const mapDispatchToProps =  ({ postEvent })
+ const mapDispatchToProps =  ({ deleteEvent })
 
 
-export default connect(null, null)(
+export default connect(null, mapDispatchToProps)(
   reduxForm({ validate, form: 'eventShowForm' })(EventsShow)
 )
 
